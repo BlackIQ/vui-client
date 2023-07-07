c756036765c3bc95830af79894685462541ce058
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-07-07 07:20:14 +0330

    Update some stuff

--- src/config/form/form.config.js
+++ src/config/form/form.config.js
@@ -1,4 +1,23 @@
 const forms = {
+  auth: {
+    username: {
+      type: "text",
+      label: "نام کاربری",
+      placeholder: "نام کاربری را وارد کنید",
+      secure: false,
+    },
+    password: {
+      type: "number",
+      label: "رمز ورود",
+      placeholder: "رمز ورود را وارد کنید",
+      secure: true,
+    },
+    god: {
+      type: "checkbox",
+      label: "ورود مدیر",
+      placeholder: "جهت ورود به عنوان مدیر تیک را بزنید",
+    },
+  },
   user: {
     username: {
       type: "text",
