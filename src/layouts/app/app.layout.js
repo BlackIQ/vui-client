66819487f89ad44c404998f1ef17ff34435751aa
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-07-07 03:40:17 +0330

    Laypout

---
+++ src/layouts/app/app.layout.js
@@ -0,0 +1,34 @@
+import { Container } from "@mui/material";
+
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+
+import { setUser } from "@/redux/actions/user";
+import API from "@/api";
+
+const AppLayout = ({ children }) => {
+  const { token, user } = useSelector((state) => state);
+
+  const dispatch = useDispatch();
+
+  const getData = async () => {
+    try {
+      const data = await API.get(`users/${user._id}`);
+
+      dispatch(setUser(data.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (token) {
+      API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      // getData();
+    }
+  }, []);
+
+  return <Container>{children}</Container>;
+};
+
+export default AppLayout;
